906eaf222d78b196186909a8ec5a103b18747389
Hariton-Andrei-Marius <Andrei.Hariton@scai-consulting.grupposcai.it>
2018-02-20 15:19:15 +0100

    Aggiunti test sulla classe RestApi

---
+++ src/test/java/start/RestApiTest.java
@@ -0,0 +1,64 @@
+package start.tests;
+
+import static org.junit.Assert.*;
+
+import java.net.URISyntaxException;
+
+import org.junit.Test;
+
+import start.modules.RestApi;
+
+public class RestApiTest
+{
+	@Test
+	public void testWeatherURI()
+	{
+		try
+		{
+			assert(RestApi.getWeatherURI("bologna").toString().length() == 13);
+		}
+		catch (URISyntaxException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testCityDetailsURI()
+	{
+		try
+		{
+			assert(RestApi.getCityDetailsURI("bologna").toString().length() == 13);
+		}
+		catch (URISyntaxException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testImagesURI()
+	{
+		try
+		{
+			assert(RestApi.getImagesURI("bologna").toString().length() == 17);
+		}
+		catch (URISyntaxException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testForecastURI()
+	{
+		try
+		{
+			assert(RestApi.getForecastURI("bologna").toString().length() == 13);
+		}
+		catch (URISyntaxException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
